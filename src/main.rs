7cf350835ebf25e6716c76ce99cccb5843cd7565
Thomas Griffiths <tomgriffiths231@gmail.com>
2022-09-12 01:47:08 +0100

    Split echo process into seperate function

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
-use std::net::{TcpListener};
+use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{Write, Result, Read};
-
+fn echo(stream: &mut TcpStream, buf: &mut [u8]) -> Result<()> {
+    stream.read(buf)?;
+    stream.write(buf)?;
+    stream.shutdown(Shutdown::Both)?;
+    Ok(())
+}
 fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:12346")?;
     println!("Bound!");
     let mut buffer: [u8; 1024] = [0;1024];
     for stream in listener.incoming() {
-        println!("Client connected, writing");
-        let mut stream = stream?;
-        println!("Converted stream var");
-        stream.read(&mut buffer)?;
+        let res = echo(&mut stream?, &mut buffer);
+        if res.is_err(){
+            eprintln!("Error {}", res.unwrap_err());
+            continue;
+        }
         println!("Received {}", String::from_utf8_lossy(&buffer));
-        stream.write(&buffer)?;
+        if buffer == [0; 1024] {
+            break;
+        }
+        buffer.fill(0);
     }
+    println!("EXITING");
     Ok(())
 }
